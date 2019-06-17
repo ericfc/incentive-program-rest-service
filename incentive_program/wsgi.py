70d9595ea5f18e0fb4fe4e06fb6cecd448d906c7
Eric Carrillo <ericfc@utexas.edu>
2019-06-17 12:55:44 -0500

    Added skeleton code for project

---
+++ incentive_program/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for incentive_program project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incentive_program.settings")
+
+application = get_wsgi_application()
