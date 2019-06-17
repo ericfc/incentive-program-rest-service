2dc73d483fc097c901551a3473eadf45f584478d
Eric Carrillo <ericfc@utexas.edu>
2019-06-17 14:19:32 -0500

    Reworked model fields

---
+++ incentive_program/migrations/0002_auto_20190617_1917.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('incentive_program', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='payments',
+            name='x',
+        ),
+        migrations.RemoveField(
+            model_name='payments',
+            name='y',
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='ccn',
+            field=models.IntegerField(null=True, db_column='CCN', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='city',
+            field=models.CharField(max_length=256, null=True, db_column='City', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='county',
+            field=models.CharField(max_length=256, null=True, db_column='County', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='fid',
+            field=models.IntegerField(serialize=False, primary_key=True, db_column='FID'),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='last_payment_criteria',
+            field=models.CharField(blank=True, max_length=256, null=True, db_column='Last_Payment_Criteria', choices=[('AIU', 'AIU'), ('MU', 'MU')]),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='last_payment_year',
+            field=models.IntegerField(null=True, db_column='Last_Payment_Year', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='last_program_year',
+            field=models.IntegerField(null=True, db_column='Last_Program_Year', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='latitude',
+            field=models.DecimalField(null=True, decimal_places=6, max_digits=9, db_column='LATITUDE', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='longitude',
+            field=models.DecimalField(null=True, decimal_places=6, max_digits=9, db_column='LONGITUDE', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='medicaid_ep_hospital_type',
+            field=models.CharField(blank=True, max_length=256, null=True, db_column='Medicaid_EP_Hospital_Type', choices=[('Acute Care Hospitals', 'Acute Care Hospitals'), ("Children's Hospitals", "Children's Hospitals")]),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='most_recent_disbursement_amount',
+            field=models.IntegerField(null=True, db_column='Most_Recent_Disbursement_Amount', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='npi',
+            field=models.IntegerField(null=True, db_column='NPI', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='payment_year_number',
+            field=models.IntegerField(null=True, db_column='Payment_Year_Number', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='program_type',
+            field=models.CharField(blank=True, max_length=256, null=True, db_column='Program_Type', choices=[('Medicaid', 'Medicaid'), ('Medicare/Medicaid', 'Medicare/Medicaid')]),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='provider_name',
+            field=models.CharField(max_length=256, null=True, db_column='Provider_Name', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='state',
+            field=models.CharField(max_length=256, null=True, db_column='State', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='street_address',
+            field=models.CharField(max_length=256, null=True, db_column='Street_Address', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='total_payments',
+            field=models.IntegerField(null=True, db_column='Total_payments', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='payments',
+            name='zip_code',
+            field=models.CharField(max_length=5, null=True, db_column='Zip_Code', blank=True),
+        ),
+    ]
--- incentive_program/models.py
+++ incentive_program/models.py
@@ -4,70 +4,127 @@ from django.db import models
 
 
 class Payments(models.Model):
-    fid = models.TextField(
+    ACUTE_CARE_HOSPITALS = 'Acute Care Hospitals'
+    CHILDRENS_HOSPITALS = 'Children\'s Hospitals'
+    HOSPITAL_TYPE_CHOICES =(
+        (ACUTE_CARE_HOSPITALS, ACUTE_CARE_HOSPITALS),
+        (CHILDRENS_HOSPITALS, CHILDRENS_HOSPITALS),
+    )
+    MEDICARE_MEDICAID = 'Medicare/Medicaid'
+    MEDICAID = 'Medicaid'
+    PROGRAM_TYPE_CHOICES = (
+        (MEDICAID, MEDICAID),
+        (MEDICARE_MEDICAID, MEDICARE_MEDICAID),
+    )
+    MU_CRITERIA = 'MU'
+    AIU_CRITERIA = 'AIU'
+    LAST_PAYMENT_CRITERIA_CHOICES = (
+        (AIU_CRITERIA, AIU_CRITERIA),
+        (MU_CRITERIA, MU_CRITERIA),
+    )
+
+    fid = models.IntegerField(
         primary_key=True,
         db_column='FID',
     )
-    x = models.TextField(db_column='X', blank=True, null=True)
-    y = models.TextField(db_column='Y', blank=True, null=True)
-    provider_name = models.TextField(
+    provider_name = models.CharField(
         db_column='Provider_Name',
+        max_length=256,
         blank=True,
         null=True,
     )
-    npi = models.TextField(db_column='NPI', blank=True, null=True)
-    ccn = models.TextField(db_column='CCN', blank=True, null=True)
-    medicaid_ep_hospital_type = models.TextField(
+    npi = models.IntegerField(db_column='NPI', blank=True, null=True)
+    ccn = models.IntegerField(db_column='CCN', blank=True, null=True)
+    medicaid_ep_hospital_type = models.CharField(
         db_column='Medicaid_EP_Hospital_Type',
+        choices=HOSPITAL_TYPE_CHOICES,
+        max_length=256,
         blank=True,
         null=True,
     )
-    street_address = models.TextField(
+    street_address = models.CharField(
         db_column='Street_Address',
+        max_length=256,
         blank=True,
         null=True,
     )
-    city = models.TextField(db_column='City', blank=True, null=True)
-    county = models.TextField(db_column='County', blank=True, null=True)
-    state = models.TextField(db_column='State', blank=True, null=True)
-    zip_code = models.TextField(db_column='Zip_Code', blank=True, null=True)
-    payment_year_number = models.TextField(
+    city = models.CharField(
+        db_column='City',
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+    county = models.CharField(
+        db_column='County',
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+    state = models.CharField(
+        db_column='State',
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+    zip_code = models.CharField(
+        db_column='Zip_Code',
+        max_length=5,
+        blank=True,
+        null=True,
+    )
+    payment_year_number = models.IntegerField(
         db_column='Payment_Year_Number',
         blank=True,
         null=True,
     )
-    program_type = models.TextField(
+    program_type = models.CharField(
         db_column='Program_Type',
+        choices=PROGRAM_TYPE_CHOICES,
+        max_length=256,
         blank=True,
         null=True,
     )
-    total_payments = models.TextField(
+    total_payments = models.IntegerField(
         db_column='Total_payments',
         blank=True,
         null=True,
     )
-    last_program_year = models.TextField(
+    last_program_year = models.IntegerField(
         db_column='Last_Program_Year',
         blank=True,
         null=True,
     )
-    last_payment_year = models.TextField(
+    last_payment_year = models.IntegerField(
         db_column='Last_Payment_Year',
         blank=True, 
         null=True,
     )
-    last_payment_criteria = models.TextField(
+    last_payment_criteria = models.CharField(
         db_column='Last_Payment_Criteria',
+        choices=LAST_PAYMENT_CRITERIA_CHOICES,
+        max_length=256,
         blank=True,
         null=True,
     )
-    most_recent_disbursement_amount = models.TextField(
+    most_recent_disbursement_amount = models.IntegerField(
         db_column='Most_Recent_Disbursement_Amount',
         blank=True,
         null=True,
     )
-    longitude = models.TextField(db_column='LONGITUDE', blank=True, null=True)
-    latitude = models.TextField(db_column='LATITUDE', blank=True, null=True)
+    longitude = models.DecimalField(
+        db_column='LONGITUDE',
+        blank=True,
+        null=True,
+        max_digits=9,
+        decimal_places=6,
+    )
+    latitude = models.DecimalField(
+        db_column='LATITUDE',
+        blank=True,
+        null=True,
+        max_digits=9,
+        decimal_places=6,
+    )
 
     class Meta:
         db_table = 'payments'
